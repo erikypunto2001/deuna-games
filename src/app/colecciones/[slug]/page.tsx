32a2e6f7ed889e2f5b8a3cad9195177303bd5636
erikypunto2001 <erikypunto2001@gmail.com>
2026-09-26 07:36:57 -0300

    Corregir contrato de breadcrumbs públicos

--- src/app/colecciones/[slug]/page.tsx
+++ src/app/colecciones/[slug]/page.tsx
@@ -166,6 +166,7 @@ export default async function CollectionDetailPage({
           }
         >
           <PublicBreadcrumb
+            className=""
             currentLabel={
               collection.title
             }
